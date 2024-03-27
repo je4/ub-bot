69303c74108779ce42c2e81543dc05897d7bf414
Juergen Enge <you@example.com>
2024-03-27 18:29:55 +0100

    initial commit

---
+++ cmd/bot/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+)
+
+func main() {
+	discord, err := discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))
+	if err != nil {
+		panic(err)
+	}
+	discord.AddHandler(newMessage)
+	discord.Open()
+	defer discord.Close() // close session, after function termination
+
+	// keep bot running untill there is NO os interruption (ctrl + C)
+	fmt.Println("Bot running....")
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+}
+
+func newMessage(discord *discordgo.Session, message *discordgo.MessageCreate) {
+	log.Printf("Message from %s: %s", message.Author.Username, message.Content)
+	/* prevent bot responding to its own message
+	this is achived by looking into the message author id
+	if message.author.id is same as bot.author.id then just return
+	*/
+	if message.Author.ID == discord.State.User.ID {
+		return
+	}
+
+	// respond to user message if it contains `!help` or `!bye`
+	switch {
+	case strings.Contains(message.Content, "!help"):
+		discord.ChannelMessageSend(message.ChannelID, "Hello World😃")
+	case strings.Contains(message.Content, "!bye"):
+		discord.ChannelMessageSend(message.ChannelID, "Good Bye👋")
+		// add more cases if required
+	}
+
+}

d7298e6766ff017c4e1e370bffed2fc5e134205a
Juergen Enge <you@example.com>
2024-03-28 15:52:36 +0100

    functions

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -9,24 +9,83 @@ import (
 	"strings"
 )
 
+const GUILD_ID = "1222591253255032913"
+const APP_ID = "1222592521310437446"
+
+var (
+	commands = []discordgo.ApplicationCommand{
+		{
+			Name:        "ping",
+			Description: "Replies with Pong!",
+		},
+	}
+
+	commandsHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
+		"ping": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "Pong!",
+				},
+			})
+		},
+	}
+)
+
 func main() {
 	discord, err := discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))
 	if err != nil {
 		panic(err)
 	}
 	discord.AddHandler(newMessage)
+	discord.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+		fmt.Println("Bot is up!")
+		for _, guild := range r.Guilds {
+			fmt.Printf("Guild: %s\n", guild.ID)
+		}
+	})
+
+	discord.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		if h, ok := commandsHandlers[i.ApplicationCommandData().Name]; ok {
+			h(s, i)
+		}
+	})
+
+	cmdIDs := make(map[string]string, len(commands))
+
+	for _, cmd := range commands {
+		rcmd, err := discord.ApplicationCommandCreate(APP_ID, GUILD_ID, &cmd)
+		if err != nil {
+			log.Fatalf("Cannot create slash command %q: %v", cmd.Name, err)
+		}
+
+		cmdIDs[rcmd.ID] = rcmd.Name
+
+	}
+	u, err := discord.User("@me")
+	if err != nil {
+		fmt.Println("Failed getting current User:", err)
+		return
+	}
+	fmt.Println("Bot user:", u.Username)
+
 	discord.Open()
 	defer discord.Close() // close session, after function termination
 
-	// keep bot running untill there is NO os interruption (ctrl + C)
-	fmt.Println("Bot running....")
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	<-c
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	<-stop
+	log.Println("Graceful shutdown")
+
+	for id, name := range cmdIDs {
+		err := discord.ApplicationCommandDelete(APP_ID, GUILD_ID, id)
+		if err != nil {
+			log.Fatalf("Cannot delete slash command %q: %v", name, err)
+		}
+	}
 }
 
 func newMessage(discord *discordgo.Session, message *discordgo.MessageCreate) {
-	log.Printf("Message from %s: %s", message.Author.Username, message.Content)
 	/* prevent bot responding to its own message
 	this is achived by looking into the message author id
 	if message.author.id is same as bot.author.id then just return
@@ -34,6 +93,7 @@ func newMessage(discord *discordgo.Session, message *discordgo.MessageCreate) {
 	if message.Author.ID == discord.State.User.ID {
 		return
 	}
+	log.Printf("%s: %s", message.Author.Username, message.Content)
 
 	// respond to user message if it contains `!help` or `!bye`
 	switch {
